a7ebd89be1957fc392f86a3156679353a30f85ec
Admin <admin@macbook-pro-admin.local>
2019-01-23 20:43:02 +0500

    Initial Commit

---
+++ Chat_With_Translator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chat_With_Translator
+//
+//  Created by Admin on 23/01/2019.
+//  Copyright © 2019 xelwow98. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
